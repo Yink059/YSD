661f08c9ee4b9298d0c62a4e5caa666cd96ce707
Brian <b.d.sandford@gmail.com>
2024-03-13 17:00:40 -0700

    1

--- PhobGCC/extras/ysd.h
+++ PhobGCC/extras/ysd.h
@@ -3,42 +3,66 @@
 #ifndef EXTRAS_YSD_H
 #define EXTRAS_YSD_H
 
-namespace ysd {
-
+namespace ysd
+{
 
 	ExtrasSlot extrasYsdConfigSlot = EXTRAS_LEFT;
-	
-	enum YsdSettings{
+
+	float defaultOffset = -54.0;
+
+	enum YsdSettings
+	{
 		YSD_SETTING_ENABLE,
-		YSD_SETTING_UNUSED1,
+		YSD_SETTING_OFFSET,
 		YSD_SETTING_UNUSED2,
 		YSD_SETTING_UNUSED3
 	};
 
-	enum YsdSettingEnable{
+	enum YsdSettingEnable
+	{
 		YSD_DISABLED,
 		YSD_ENABLED
 	};
 
-
-	bool check_enabled(const IntOrFloat config[]) {
-		if (config[YSD_SETTING_ENABLE].intValue != YSD_ENABLED) {
-			return false; //do nothing, keep hyst enabled
+	bool check_enabled(const IntOrFloat config[])
+	{
+		if (config[YSD_SETTING_ENABLE].intValue != YSD_ENABLED)
+		{
+			return false;
 		}
 		return true;
 	}
 
-	bool toggle(IntOrFloat config[]) {
-		int& enabled = config[YSD_SETTING_ENABLE].intValue;
-		if (enabled != YSD_DISABLED){
+	void config(IntOrFloat config[], Cardinals dpad)
+	{
+		if (dpad == Cardinals.d) {
+			setExtrasSettingFloat(extrasYsdConfigSlot, YSD_SETTING_OFFSET, getOffset() + 1.0);
+		}
+		if (dpad == Cardinals.u) {
+			setExtrasSettingFloat(extrasYsdConfigSlot, YSD_SETTING_OFFSET, getOffset() - 1.0);
+		}
+	}
+
+	float getOffset() {
+		return getExtrasSettingFloat(extrasYsdConfigSlot, YSD_SETTING_OFFSET)
+	}
+
+	bool toggle(IntOrFloat config[])
+	{
+		int &enabled = config[YSD_SETTING_ENABLE].intValue;
+		if (enabled != YSD_DISABLED)
+		{
 			enabled = YSD_DISABLED;
-		} else {
+		}
+		else
+		{
 			enabled = YSD_ENABLED;
 		}
+		
 		setExtrasSettingInt(extrasYsdConfigSlot, YSD_SETTING_ENABLE, enabled);
 		return (enabled == YSD_ENABLED);
 	}
 
 };
 
-#endif //EXTRAS_ESS_H
+#endif // EXTRAS_ESS_H
